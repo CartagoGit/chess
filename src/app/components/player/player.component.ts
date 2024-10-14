ec0d1dd4d16147c3d554024916bb5cbb88374b1f
Cartago <cartago.relaxingcup@gmail.com>
2024-10-15 00:10:46 +0200

    Se añaden parametros básicos del jugador.

--- src/app/components/player/player.component.ts
+++ src/app/components/player/player.component.ts
@@ -1,14 +1,19 @@
-import { CommonModule } from "@angular/common";
+import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { IColor } from '@interfaces/board.types';
 
 @Component({
   selector: 'chess-player',
   standalone: true,
-  imports: [
-    CommonModule,
-  ],
+  imports: [CommonModule],
   templateUrl: './player.component.html',
   styleUrl: './player.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class PlayerComponent { }
+export class PlayerComponent {
+  public isTurn: boolean = false;
+  public color!: IColor;
+  public time: number = 0;
+  public name: string = 'Player';
+  public elo: number = 0;
+}
